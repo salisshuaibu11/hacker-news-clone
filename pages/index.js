a232ed73f913c98a71b58da2f395b9d3a3cc14c2
Salisu Shuaibu <salisshuaibu11@gmail.com>
2021-11-09 12:47:07 +0100

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class Index extends Component {
+    render() {
+        return (
+            <div>Hacker News</div>
+        )
+    }
+}
+
+export default Index;
